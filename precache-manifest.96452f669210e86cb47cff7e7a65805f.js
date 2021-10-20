935e64a50e83da8315803a87a6ac4ed6e29fea39
omar212 <omarelnagdy16@gmail.com>
2021-10-20 14:57:52 -0400

    Updates

---
+++ precache-manifest.96452f669210e86cb47cff7e7a65805f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b707f59cd1e036ab27a2",
+    "url": "/HerQuest/static/css/main.06557ef9.chunk.css"
+  },
+  {
+    "revision": "b707f59cd1e036ab27a2",
+    "url": "/HerQuest/static/js/main.b707f59c.chunk.js"
+  },
+  {
+    "revision": "ea302e4ebd5a18548fa2",
+    "url": "/HerQuest/static/js/1.ea302e4e.chunk.js"
+  },
+  {
+    "revision": "0b290ab9714ceeb0ea66",
+    "url": "/HerQuest/static/js/runtime~main.0b290ab9.js"
+  },
+  {
+    "revision": "ac1eaeec0259aba191c07ea6398e357c",
+    "url": "/HerQuest/static/media/hq_logo-01.ac1eaeec.png"
+  },
+  {
+    "revision": "c907466e0644220644214f8968497ecf",
+    "url": "/HerQuest/static/media/snapcodeAr.c907466e.png"
+  },
+  {
+    "revision": "8f3628d2659abc557ce38bf08f5563fb",
+    "url": "/HerQuest/static/media/snapcodeCS.8f3628d2.png"
+  },
+  {
+    "revision": "2f706f7a1cb05e9e698ed7dfa82f3fbb",
+    "url": "/HerQuest/static/media/snapcodeME.2f706f7a.png"
+  },
+  {
+    "revision": "501f683a254f18c0463d9fe50ad04a0c",
+    "url": "/HerQuest/index.html"
+  }
+];
